43a8f3573be666d31924ac489ff1359dadb56e8f
arthurpfeilsticker <arthurlfpfeil@gmail.com>
2024-12-02 21:02:22 -0300

    feat: created mentors entity/module/controller/service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { MentorsModule } from './modules/mentors/mentors.module';
 
 @Module({
-  imports: [],
+  imports: [
+    //ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    MentorsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/mentors/mentors.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Mentor } from 'src/entities/mentor.entity';
+import { MentorsService } from 'src/services/mentors/mentors.service';
+import { MentorsController } from 'src/controllers/mentors/mentors.controller';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Mentor])],
+    providers: [MentorsService],
+    controllers: [MentorsController],
+    exports: [MentorsService],
+})
+export class MentorsModule {}
