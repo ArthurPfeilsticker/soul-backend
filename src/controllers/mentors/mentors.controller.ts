79a9621f109b36c7d2ef29563f9b5bd4b66a9041
arthurpfeilsticker <arthurlfpfeil@gmail.com>
2024-12-02 23:39:47 -0300

    feat: mentors entity

--- src/controllers/mentors/mentors.controller.ts
+++ src/controllers/mentors/mentors.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { MentorsService } from 'src/services/mentors/mentors.service';
+import { Mentor } from 'src/entities/mentor.entity';
+import { MentorsModule } from 'src/modules/mentors/mentors.module';
 
 @Controller('mentors')
-export class MentorsController {}
+export class MentorsController {
+    constructor(private readonly mentorService: MentorsService){}
+
+    //creates a new mentor
+    @Post()
+    async create(@Body() mentorData: Mentor): Promise<Mentor>{
+        return this.mentorService.create(mentorData);
+    }
+
+    //gets all the signed up mentors
+    @Get()
+    async findAll(): Promise<Mentor[]>{
+        return this.mentorService.findAll();
+    }
+
+    //gets a mentor by his ID
+    @Get(':id')
+    async findOne(@Param('id') id: number): Promise<Mentor>{
+        return this.mentorService.findOne(id);
+    }
+
+    //updates a mentor searching by his ID
+    @Put(':id')
+    async update(@Param('id') id: number, @Body() mentorData: Mentor): Promise<Mentor>{
+        return this.mentorService.update(id, mentorData);
+    }
+
+    //deletes a mentor searching by his ID
+    @Delete(':id')
+    async remove(@Param('id') id: number): Promise<void>{
+        return this.mentorService.remove(id);
+    }
+}
--- src/services/mentors/mentors.service.ts
+++ src/services/mentors/mentors.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { NumericType, Repository } from 'typeorm';
+import { Mentor } from 'src/entities/mentor.entity';
 
 @Injectable()
-export class MentorsService {}
+export class MentorsService {
+    constructor(
+        @InjectRepository(Mentor)
+        private readonly mentorRepository: Repository<Mentor>,
+    ){}
+
+    //creates new mentor
+    async create(mentorData: Mentor): Promise<Mentor>{
+        const mentor = this.mentorRepository.create(mentorData);
+        return this.mentorRepository.save(mentor);
+    }
+
+    //gets all signed up mentors
+    async findAll(): Promise<Mentor[]>{
+        return this.mentorRepository.find();
+    }
+
+    //gets a mentor by his ID
+    async findOne(id: number): Promise<Mentor>{
+        const mentor = await this.mentorRepository.findOne({where: {id}});
+        if(!mentor){
+            throw new NotFoundException(`Mentor with especified ID ${id} not found`);
+        }
+        return mentor;
+    }
+
+    //updates a mentor searching by his ID
+    async update(id: number, mentorData: Mentor): Promise<Mentor>{
+        const mentor = await this.findOne(id);
+        Object.assign(mentor, mentorData);
+        return this.mentorRepository.save(mentor);
+    }
+
+    //deletes a mentor searching by his ID
+    async remove(id: number): Promise<void>{
+        const mentor = await this.findOne(id);
+        await this.mentorRepository.remove(mentor);
+    }
+}
