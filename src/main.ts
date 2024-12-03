150d1299b8e9a1cccf0c93fed6cd4fc17e5518c8
arthurpfeilsticker <arthurlfpfeil@gmail.com>
2024-12-03 00:24:40 -0300

    fix: deployment issues

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://soul-backend-f8jvvukjm-arthurpfeilstickers-projects.vercel.app',
+  })
+
   await app.listen(3000);
 }
 bootstrap();
